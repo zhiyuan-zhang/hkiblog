b780dc8516182e64f9c7edac841f8c23e67de79c
zhanghao <913314986@qq.com>
2020-05-14 18:22:54 +0800

    Auto

--- aaa.sh
+++ aaa.sh
@@ -4,3 +4,11 @@ git commit -m "Auto"
 git push origin dev
 hexo clean && hexo g
 hexo d
+# Xmind
+cd /Users/apple/Documents/Object/马老师/ximd
+git pull origin master
+git add --all
+git commit -m "Auto"
+git push origin master
+
+
